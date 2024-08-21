1f625b4389f06cde87f604954aa4d0e3a9dee22d
kusa lachisa <kusalechisac@gmail.com>
2024-08-22 00:30:24 +0300

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
-import AppRoutes from './AppRoutes';
-import Header from './components/Header/Header';
-import Loading from './components/Loading/Loading';
-import { useLoading } from './hooks/useLoading';
-import { setLoadingInterceptor } from './interceptors/loadingInterceptor';
-import { useEffect } from 'react';
+import AppRoutes from "./AppRoutes";
+import Header from "./components/Header/Header";
+import Loading from "./components/Loading/Loading";
+import { useLoading } from "./hooks/useLoading";
+import { setLoadingInterceptor } from "./interceptors/loadingInterceptor";
+import { useEffect } from "react";
 
 function App() {
   const { showLoading, hideLoading } = useLoading();
 
   useEffect(() => {
     setLoadingInterceptor({ showLoading, hideLoading });
-  }, []);
+  }, [showLoading, hideLoading]); // Added dependencies here
 
   return (
     <>
